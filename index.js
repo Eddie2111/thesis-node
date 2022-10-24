1fd1fe8e10c21216e38f7d9d3068d8d35ae15e7a
ASM Tareq Mahmood <asm.tareq.mahmood@g.bracu.ac.bd>
2022-10-24 19:09:48 +0600

    push [0.0.1]

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+const port = 3000;
+
+// messages
+const listen = require('./data/messages');
+
+//configs
+const cookie = require('./data/config');
+
+//environment
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use( express.static( "public" ) );
+app.use(session(cookie));
+
+//database
+const mongo = require('./model/mongoose');
+
+const index = require('./routes/index');
+const recieve = require('./routes/recieve');
+const bulk = require('./routes/bulk');
+const showdata = require('./routes/showdata');
+
+app.use('/',index);
+app.use('/recieve',recieve);
+app.use('/bulk',bulk);
+app.use('/showdata',showdata);
+
+// error handling
+const {errorRoute} = require ("./data/messages");
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.send(errorRoute);
+});
+
+
+app.listen(port,listen);
---
+++ routes/recieve.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+const app     = express();
+const router  = express.Router();
+const dataSchema = require('../model/schema');
+app.set('view engine','ejs');
+const data = {
+    title: "welcome",
+    message: "data came from node backend but changed",
+    version: "10.10.10",
+    page:"/very very first test page!"
+};
+
+
+router
+    .route('/')
+    .get((req,res)=>{
+        res.render('index',data);
+    })
+    .post((req,res)=>{
+        console.log(req.body.sentence);
+        const data = new dataSchema({
+            id: Date.now(),
+            message: req.body.sentence
+        });
+        data.save()
+            .then(data =>{
+                res.redirect('/');
+            })
+            .catch(err =>{
+                res.json({message: err});
+            });
+    }
+    );
+
+module.exports = router;
+
+
